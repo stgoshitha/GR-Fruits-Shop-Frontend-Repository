c14c2a63c5338385c7df5c82fd6f7a52f5ac77f8
stgoshitha <wkgoshithapremachandra@gmail.com>
2024-11-30 16:24:54 +0530

    [UPDATE] Add routing for pages and integrate navigation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Header from './components/Header'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Fruits from './pages/Fruits'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Verify from './pages/Verify'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fruits" element={<Fruits />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/placeOreder" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/verify" element={<Verify />} />
+      </Routes>
+    </main>
   )
 }
 
