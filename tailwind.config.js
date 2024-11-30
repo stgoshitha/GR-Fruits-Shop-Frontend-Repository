c7e33d89af1be30a8c07a181e74d7564e5e8ab96
stgoshitha <wkgoshithapremachandra@gmail.com>
2024-11-30 16:37:56 +0530

    [UPDATE] Update Tailwind CSS configuration with custom color palette.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f9f9f9",
+        primaryLight: "#e3f7fa",
+        secondary: "#ff8b06",
+        tertiary: "#404040",
+        gray: {
+          10: "#EEEEEE",
+          20: "#A2A2A2",
+          30: "#7B7B7B",
+          50: "#585858",
+          90: "#141414",
+        },
+      },
+    },
   },
   plugins: [],
 }
