adaea039e723c8cddebca853c734bd22d3e988a8
stgoshitha <wkgoshithapremachandra@gmail.com>
2024-12-01 03:50:59 +0530

    [FEAT](home) : Add a button for smooth scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import React, { useRef } from 'react';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import { FaArrowDown } from "react-icons/fa6";
 
 const Home = () => {
+  const featuresRef = useRef(null);
+
+  const scrollToFeatures = () => {
+    featuresRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div>
-      <Hero/>
+      <Hero />
+      <div className="flex justify-center">
+        <button 
+          onClick={scrollToFeatures} 
+          className="absolute flex justify-center items-center -my-7 px-4 py-4 w-16 h-16 bg-orange-100 text-black rounded-full transition-all duration-300 transform hover:scale-110 hover:shadow-md hover:bg-orange-200"
+        >
+          <FaArrowDown className="text-2xl animate-bounce" />
+        </button>
+      </div>
+      <hr className="h-1" />
+      <div ref={featuresRef}>
+        <Features />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
