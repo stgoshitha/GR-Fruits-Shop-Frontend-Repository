fdd8f4edcf07baa499f5996e5dbf68852f66ee8a
stgoshitha <wkgoshithapremachandra@gmail.com>
2024-12-01 14:02:34 +0530

    [UPDATE] : update the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 
+
 const About = () => {
   return (
     <div>
       <section className='min-h-[700px] bg-center bg-cover flex justify-center items-center'>
           <div className="p-6 md:p-10 lg:p-20 ">
+
             <span className="text-4xl md:text-6xl font-semibold">About Us</span><br/><br/>
-            <h1 className="text-xl md:text-3xl text-center  antialiased  leading-relaxed">
-              <span className="text-orange-700">Welcome to Fresh4You!</span> We're your go-to source for top-quality fruits and unbeatable
+            <h1 className="text-xl md:text-3xl text-center  antialiased  leading-relaxed">Welcome to
+              <span className="text-orange-700"> 
+                  <span className='text-5xl'> GR</span>
+                  <span className='text-secondary text-3xl font-bold'>Fruits.</span>
+              </span> 
+              We're your go-to source for top-quality fruits and unbeatable
               freshness. Founded by Asidu, we're passionate about providing you with the best fruits
               for every occasion. With fair prices and a commitment to social responsibility,
-              Fresh4You is your trusted partner for vibrant living. Explore our website and
+              <span className="text-orange-700"> 
+                  <span className='text-5xl'>GR</span>
+                  <span className='text-secondary text-3xl font-bold'>Fruits. </span>
+              </span>
+              is your trusted partner for vibrant living. Explore our website and
               experience the taste of freshness today!
               <br /><br />
               <hr className="h- border-t border-gray-400" />
-              <br />
-              <span className="text-5xl">🍎🍌🍇</span>
+              <div className='mt-5'><span className="text-5xl">🍎🍌🍇</span></div>
             </h1>
-            <br />
+            
           </div>
       </section>
     </div>
