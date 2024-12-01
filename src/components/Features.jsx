6908e096325a56fda9df6ff4406cf877888cbd95
stgoshitha <wkgoshithapremachandra@gmail.com>
2024-12-01 11:41:34 +0530

    [UPDATE](feature-section) : change the colors and size of Feature Card

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,37 +14,37 @@ const Features = () => {
         <div className="flex flex-col items-center justify-center gap-16 px-4 2xl:h-[600px] sm:h-[1000px]">
           <div className="flex flex-wrap gap-12 justify-center">
             {/* Feature Card */}
-            <div className="flex flex-col items-center justify-center gap-4 shadow-xl p-8 sm:p-10 md:p-12 lg:p-16 bg-white rounded-lg max-w-[350px] duration-700 transform hover:scale-110 hover:shadow-2xl">
+            <div className="flex flex-col items-center justify-center gap-4 shadow-xl p-8 sm:p-10 md:p-12 lg:p-16 bg-orange-600 text-white rounded-lg max-w-[450px] duration-700 transform hover:scale-110 hover:shadow-2xl hover:bg-white hover:text-gray-700 hover:border-4 hover:border-orange-600">
               <img src={shippingfast} alt="" className="h-16 w-16 sm:h-20 sm:w-20" />
               <div className="flex items-center justify-center flex-col">
                 <h5 className="text-xl md:text-2xl lg:text-[25px] font-bold">Fresh Fruit</h5>
                 <hr className="w-16 md:w-20 bg-secondary h-1 rounded-full border-none" />
               </div>
-              <p className="text-sm md:text-base lg:text-lg text-center text-gray-700">
+              <p className="text-sm md:text-base lg:text-lg text-center hover:text-gray-700">
                 Indulge in freshness. Hand-picked and delivered for you to enjoy. Taste the difference today.
               </p>
             </div>
 
             {/* Feature Card */}
-            <div className="flex flex-col items-center justify-center gap-4 shadow-xl p-8 sm:p-10 md:p-12 lg:p-16 bg-white rounded-lg max-w-[350px] duration-700 transform hover:scale-110 hover:shadow-2xl">
+            <div className="flex flex-col items-center justify-center gap-4 shadow-xl p-8 sm:p-10 md:p-12 lg:p-16 bg-yellow-400 text-white rounded-lg max-w-[450px] duration-700 transform hover:scale-110 hover:shadow-2xl hover:bg-white hover:text-gray-700 hover:border-4 hover:border-yellow-400">
               <img src={shippingfast} alt="" className="h-16 w-16 sm:h-20 sm:w-20" />
               <div className="flex items-center justify-center flex-col">
                 <h5 className="text-xl md:text-2xl lg:text-[25px] font-bold">Fast Delivery</h5>
                 <hr className="w-16 md:w-20 bg-secondary h-1 rounded-full border-none" />
               </div>
-              <p className="text-sm md:text-base lg:text-lg text-center text-gray-700">
+              <p className="text-sm md:text-base lg:text-lg text-center ">
                 Experience lightning-fast delivery with us. Get what you need, when you need it. Your satisfaction, our priority.
               </p>
             </div>
 
             {/* Feature Card */}
-            <div className="flex flex-col items-center justify-center gap-4 shadow-xl p-8 sm:p-10 md:p-12 lg:p-16 bg-white rounded-lg max-w-[350px] duration-700 transform hover:scale-110 hover:shadow-2xl">
+            <div className="flex flex-col items-center justify-center gap-4 shadow-xl p-8 sm:p-10 md:p-12 lg:p-16 bg-lime-600 text-white rounded-lg max-w-[450px] duration-700 transform hover:scale-110 hover:shadow-2xl hover:bg-white hover:text-gray-700 hover:border-4 hover:border-lime-600">
               <img src={shippingfast} alt="" className="h-16 w-16 sm:h-20 sm:w-20" />
               <div className="flex items-center justify-center flex-col">
                 <h5 className="text-xl md:text-2xl lg:text-[25px] font-bold">Fruit Offer</h5>
                 <hr className="w-16 md:w-20 bg-secondary h-1 rounded-full border-none" />
               </div>
-              <p className="text-sm md:text-base lg:text-lg text-center text-gray-700">
+              <p className="text-sm md:text-base lg:text-lg text-center">
                 Discover fresh delights with our fruit offer. Premium picks at unbeatable prices. Don't miss out!
               </p>
             </div>
